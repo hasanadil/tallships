6ae314f1b85ac13570521f2c4770495b1237c8ba
Hasan Adil <@dispatch_hasan>
2015-07-01 22:11:55 -0400

    no message

--- TallShips/AppDelegate.swift
+++ TallShips/AppDelegate.swift
@@ -20,6 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
         Fabric.with([Crashlytics()])
+        
         Parse.enableLocalDatastore()
         Parse.setApplicationId("k4m0ZTMSjyOtdm5y8VM26nbBnnUAqIu5pxcHWtWg",
             clientKey: "56pOzDs37nGwT00quCVebcii7ErqpUjcUuvuYz5A")
